64427e412e08649b18e61799463ea0a56d1ad08d
riovan716 <riovansihombing3@gmail.com>
2024-11-20 15:42:38 +0700

    kompetisi

--- resources/views/kompetisiAll.blade.php
+++ resources/views/kompetisiAll.blade.php
@@ -1,31 +1,42 @@
 @extends('master')
 
 @section('section')
-<div class="text-light">
-    <div class="p-2 ">
-        <h1 class="ms-5"><strong>Kompetisi</strong></h1>
-        <br>
+<div class="container mt-5">
+    <!-- Header -->
+    <div class="text-center mb-4">
+        <h1 class="text-primary fw-bold"><strong>Kompetisi</strong></h1>
+        <hr class="w-50 mx-auto">
+    </div>
 
-        <ul style="list-style: none;">
-            @foreach ($kompetisi as $item)
-            <div class="p-2" style="border-radius: 25px; background-color: rgb(160, 170, 206);">
-                <li>
-                    <div class="border border-light pt-1 px-2 rounded-4" style="background-color:rgba(245, 245, 220, 0.144)">
-                        <h3><a href="/kompetisi/{{ $item->id }}">{{ $item->nama }}</a></h3>
-                        <p>Oleh : {{ $item->penyelenggara }}</p>
-                        <p>Batas Pendaftaran : {{ $item->tanggal }}</p>
-                    </div>
-                </li>
+    <!-- Kompetisi List -->
+    <div class="row justify-content-center">
+        @foreach ($kompetisi as $item)
+        <div class="m-2 p-2">
+            <div class="d-flex mt-2" style="background-color: rgb(160, 170, 206);">
+                <div class="card-body p-4" style="background-color: rgba(245, 245, 220, 0.144);">
+                    <h3 class="card-title fw-bold">
+                        <img src="{{ asset('asset/img/Kompetisi/' . $item->gambar) }}" alt="{{ $item->nama }}" class="img-fluid mb-3" style="max-height: 200px; object-fit: cover;">
+                        <a href="/kompetisi/{{ $item->id }}" class="text-dark text-decoration-none">
+                            {{ $item->nama }}
+                        </a>
+                    </h3>
+                    <p class="mb-1">Oleh: <strong>{{ $item->penyelenggara }}</strong></p>
+                    <p>Batas Pendaftaran: {{ \Carbon\Carbon::parse($item->tanggal)->format('d M Y') }}</p>
+                </div>
             </div>
-            <br>
-            @endforeach
-
-        </ul>
-        <div style="width: 50%; /* or any other width */
-            margin: auto;">
-            <br>
-            {{ $kompetisi->links('custom') }}
         </div>
+        @endforeach
+    </div>
+
+    <!-- Pagination -->
+    <div class="d-flex justify-content-center mt-4">
+        {{ $kompetisi->links('custom') }}
     </div>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" 
+    crossorigin="anonymous">
+</script>
+@endsection
