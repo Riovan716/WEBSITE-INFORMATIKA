698445e6df553524234cdaa761abf195119d0cd9
viktrislubis <kristrianiviktris@gmail.com>
2024-11-21 08:36:02 +0700

    fixing tampilan visimisi pada user

--- resources/views/visiMisi.blade.php
+++ resources/views/visiMisi.blade.php
@@ -1,56 +1,69 @@
 @extends('master')
 
 @section('section')
-<div class="bg-light py-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 d-flex justify-content-center align-items-center">
-                <h1 class="p-5 rounded-5"
-                    style="background: rgb(133, 128, 218);
-                    background: linear-gradient(90deg, rgba(133, 128, 218, 1) 0%, rgba(85, 85, 201, 0.6895133053221288) 10%, rgba(0, 212, 255, 1) 100%);">
-                    <b>VISI MISI</b>
-                </h1>
-            </div>
-            <div class="col-md-6">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <fieldset class="border border-primary rounded-3 p-4 "
-                                style="border-color:blue;">
-                                <legend class="float-none w-auto p-3"
-                                    style="background: rgb(0,212,255);
-                                background: linear-gradient(0deg, rgba(0,212,255,0) 0%, rgba(31,51,185,0.92) 90%);">
-                                    VISI</legend>
-                                <p><b>{{ $visi[0]->visi }}</b></p>
-                            </fieldset>
-                        </div>
-                    </div>
+    <div class="bg-light">
+        <!-- Bagian dengan Background Gambar -->
+        <div class="py-5 text-center text-white"
+            style="
+                background-image: url('/asset/img/pemandangan_2.jpg'); 
+                background-size: cover;
+                background-position: center;
+                background-repeat: no-repeat;
+                min-height: 300px; 
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            ">
+            <h1 class="p-5 rounded-5"
+                style=" 
+               display: inline-block;
+               font-size: 3rem;
+               font-weight: bold;
+               position: absolute;
+               bottom: 260px;
+               left: 10px;">
+                VISI & MISI
+            </h1>
 
-                    <div class="row">
-                        <div class="col-md-12">
-                            <br><br>
-                            <fieldset class="border border-primary rounded-3 p-4">
-                                <legend class="float-none w-auto p-3"
-                                    style="background: rgb(0,212,255);
-                                background: linear-gradient(0deg, rgba(0,212,255,0) 0%, rgba(31,51,185,0.92) 90%);">
-                                    MISI</legend>
-                                <ol>
-                                    @foreach ($misi as $item)
-                                    <b>
-                                        <li>
-                                            {{ $item->misi }}
-                                        </li>
-                                    </b>
-                                    @endforeach
-                                </ol>
-                            </fieldset>
-                            <br>
-                        </div>
-                    </div>
+        </div>
+
+        <!-- Bagian Konten -->
+        <div class="container py-5">
+            <!-- VISI -->
+            <div class="row mt-5">
+                <div class="col-md-12">
+                    <fieldset class="border border-primary rounded-3 p-4 bg-white shadow-sm">
+                        <legend class="float-none w-100 text-white rounded py-3 px-4"
+                            style="background: linear-gradient(90deg, rgba(0, 51, 102, 1) 0%, rgba(0, 153, 255, 1) 100%);">
+                            VISI
+                        </legend>
+                        <p><b>{{ $visi[0]->visi }}</b></p>
+                    </fieldset>
+                </div>
+            </div>
+
+            <!-- MISI -->
+            <div class="row mt-5">
+                <div class="col-md-12">
+                    <fieldset class="border border-primary rounded-3 p-4 bg-white shadow-sm">
+                        <legend class="float-none w-100 text-white rounded py-3 px-4"
+                            style="background: linear-gradient(90deg, rgba(0, 51, 102, 1) 0%, rgba(0, 153, 255, 1) 100%);">
+                            MISI
+                        </legend>
+                        <ol>
+                            @foreach ($misi as $item)
+                                <li>
+                                    <b>{{ $item->misi }}</b>
+                                </li>
+                            @endforeach
+                        </ol>
+                    </fieldset>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 @endsection
