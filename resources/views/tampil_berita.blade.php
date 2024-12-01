9f7b86b1de81e234687e00b43f2f456764462280
deak-20 <napitupuludeak426@gmail.com>
2024-12-01 10:52:03 +0700

    tampilan berita

--- resources/views/tampil_berita.blade.php
+++ resources/views/tampil_berita.blade.php
@@ -1,23 +1,22 @@
 @extends('master')
 
 @section('section')
+<div class="container my-5">
+    <!-- Judul Berita -->
+    <h1 class="text-center mb-4">{{ $berita->judul }}</h1>
 
-<div class="text-body container">
-    <h1 >{{ $berita->judul }}</h1>
-
+    <!-- Gambar dan Deskripsi Berita -->
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <img src="{{ asset('asset/img/Berita/' . $berita->gambar) }}" alt="{{ $berita->judul }}" class="img-fluid rounded" style="max-width: 100%; height: auto;">
+        </div>
+        <div class="col-md-6">
+            <div class="text-body">
+                <p class="text-justify">{{ $berita->isi }}</p>
+            </div>
+        </div>
+    </div>
 </div>
 
-<br><br>
-
-<div class="d-flex justify-content-center container">
-    <img src="{{ asset('asset/img/Berita/' . $berita->gambar) }}"
-                                            alt="{{ $berita->gambar }}" style="border-radius:9px 9px 0px 0;">
-</div>
-
-<br><br>
-<div class= "text-body container">
-
-    <p class="text-full">{{ $berita->isi  }}</p>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 @endsection
