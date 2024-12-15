7b32b256dc9085c2561e41bfdb7f82cb09e24296
riovan716 <riovansihombing3@gmail.com>
2024-12-16 01:12:36 +0700

    perbaikan

--- database/seeders/visiMisi.php
+++ database/seeders/visiMisi.php
@@ -15,24 +15,24 @@ class visiMisi extends Seeder
      */
     public function run()
     {
-        
+
         DB::table('misi')->insert([
 
             [
-                'misi'=> "Menyelenggarakan pendidikan teknik informatika yang bermutu, profesional dan diperhitungkan secara nasional.",
+                'misi' => "Menyelenggarakan pendidikan teknik informatika yang bermutu, profesional dan diperhitungkan secara nasional.",
             ],
             [
-                'misi'=> "Menyelenggarakan penelitian yang menghasilkan dan memanfaatkan teknologi untuk mengembangkan potensi lokal.",
+                'misi' => "Menyelenggarakan penelitian yang menghasilkan dan memanfaatkan teknologi untuk mengembangkan potensi lokal.",
             ],
             [
-                'misi'=> "Melakukan pengabdian kepada masyarakat dalam bidang teknik informatika.",
+                'misi' => "Melakukan pengabdian kepada masyarakat dalam bidang teknik informatika.",
             ],
-        ]);   
+        ]);
         DB::table('visi')->insert([
-        	'visi'=>"Menjadi program Teknik Informatika yang unggul yang berperan dalam menghasilkan dan memanfaatkan teknologi untuk mengembangkan potensi lokal bagi kemajuan bangsa pada tahun 2023.",
-        ]);   
+            'visi' => "Menjadi program Teknik Informatika yang unggul yang berperan dalam menghasilkan dan memanfaatkan teknologi untuk mengembangkan potensi lokal bagi kemajuan bangsa pada tahun 2023.",
+        ]);
         DB::table('sejarah')->insert([
-        	'sejarah'=>"<h3>Sejarah Program Studi S1-Informatika</h3><p>Program Studi S1 Teknik 
+            'sejarah' => "<h3>Sejarah Program Studi S1-Informatika</h3><p>Program Studi S1 Teknik 
             Informatika merupakan salah satu dari 4 (empat) program studi di bawah <strong>Fakultas 
             Teknik Informatika dan Elektro</strong> di Institut Teknologi Del.&nbsp;</p><p>Prodi S1 
             Informatika ini di rancang sejak tahun 2013 dan beroperasi pada tahun 2014 bersama 
@@ -42,7 +42,7 @@ class visiMisi extends Seeder
             Politeknik Informatika Del (PI Del) menjadi Institut Teknologi Del (IT Del).</p><p>&nbsp;
             </p><h3>Cakupan Ilmu</h3><p>Program Studi S1 Informatika (PSIF) di Institut Teknologi 
             Del (IT Del) memiliki cakupan bidang ilmu (<i>body of knowledge</i>) meliputi area ilmu 
-            komputer (<i>Computer Science</i>), rekayasa perangkat lunak (<>Software Engineering</
+            komputer (<i>Computer Science</i>), rekayasa perangkat lunak (Software Engineering</
             i>), keamanan siber (<i>Cybersecurity</i>), dan ilmu data (<i>Data Science</i>). Lulusan 
             dari PSIF dipersiapkan untuk dapat memiliki kemampuan analisis persoalan yang terkait 
             bidang <i>computer science</i> dan pengembangan perangkat lunak mulai dari yang 
@@ -52,6 +52,6 @@ class visiMisi extends Seeder
             diproyeksikan untuk memiliki kemampuan mengembangkan perangkat lunak yang pintar 
             menggunakan sejumlah teknik kecerdasan buatan dan secara khusus memiliki kemampuan untuk 
             menjadi <i>artificial intelligence engineer</i>.</p>",
-        ]);  
+        ]);
     }
 }
